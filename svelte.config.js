936f729f03c8e100e79b3b6ab8f5c108a656f144
BeepBot99 <74377508+BeepBot99@users.noreply.github.com>
2025-04-20 18:25:05 -0500

    Change base path

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const config = {
+const config= {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
 			fallback: '404.html'
-		})
+		}),
+		paths: {
+			base: '/umtymp-grade-calculator/'
+		}
 	}
 };
 
